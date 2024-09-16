dec23626af4d2d6e5a09830eb6c8b081c659b984
Dennis <dennisbircan@gmail.com>
2024-09-16 15:39:08 +0200

    ClassDiagram

---
+++ OOStudent5/Education.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOStudent5
+{
+    public class Education
+    {
+        public string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int ShortName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int EducationPoints
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void CreateEducation()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
